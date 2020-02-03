1ba16c50ab75d6c415f0f2f396ae51bee2b4063a
Vanessa Mauna <vmauna@aol.com>
2020-02-03 16:30:32 -0500

    Updates

---
+++ precache-manifest.0a662df2f677982d013f8c61e187ce59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5a5087d9374f39c75066a033142913",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "68dbf787ba7b715bb239",
+    "url": "/portfolio/static/css/main.c9ff8bd6.chunk.css"
+  },
+  {
+    "revision": "42f0776daac3e4943a20",
+    "url": "/portfolio/static/js/2.dac8333e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.dac8333e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68dbf787ba7b715bb239",
+    "url": "/portfolio/static/js/main.c1e7b256.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "75ea4fad25008b30c0bff11aed9173f4",
+    "url": "/portfolio/static/media/IMG_2589.75ea4fad.jpeg"
+  },
+  {
+    "revision": "472fa48721d58e8bbc03fb70d615294a",
+    "url": "/portfolio/static/media/MC-Login-SC.472fa487.png"
+  },
+  {
+    "revision": "c58f50fcb38a9ab93c6a82ff1dc991a6",
+    "url": "/portfolio/static/media/stypSC-macbook.c58f50fc.png"
+  },
+  {
+    "revision": "e579323d33651ec7b3dc404eba95d09f",
+    "url": "/portfolio/static/media/wrSC-iPhone.e579323d.png"
+  }
+]);
